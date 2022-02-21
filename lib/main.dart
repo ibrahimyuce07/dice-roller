50419397ada6ca4ab194e6007f85fdc946f02795
İbrahim Yüce <71738080+ibrahimyuce07@users.noreply.github.com>
2022-02-22 01:41:38 +0300

    rng page

--- lib/main.dart
+++ lib/main.dart
@@ -1,132 +1,147 @@
 import 'dart:math';
 import 'dart:typed_data';
 
+import 'package:dice_roller/dice_page.dart';
+import 'package:dice_roller/dnd_page.dart';
+import 'package:dice_roller/flip_page.dart';
+import 'package:dice_roller/rng_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Dice Roller',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Dice Roller'),
-    );
+    return MaterialApp(home: DemoApp());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
+class DemoApp extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _DemoAppState createState() => _DemoAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage>  {
-  int _selectedIndex = 0;
-  String url =
-      'images/flip-0.gif';
-  Widget _pic = CircularProgressIndicator();
+class _DemoAppState extends State<DemoApp> {
+  Widget _currentWidget = const Scaffold();
+  var _currentIndex = 0;
+  String url = 'images/flip-0.gif';
   List<String> coinFace = ['heads', 'tail'];
-  @override
-  void initState() {
-    super.initState();
-    setState(){
-      print('refreshing');
+
+
+  String _changeAppBarTitle() {
+    switch(_currentIndex) {
+      case 0: return 'Dice';
+      case 1: return 'DND';
+      case 2: return 'Flip';
+      case 3: return 'Random Number Generator';
+      default: return 'Dice';
+    }
+  }
+
+  Color _changeAppBarColor() {
+    switch(_currentIndex) {
+      case 0: return Colors.red;
+      case 1: return Colors.green;
+      case 2: return Colors.purple;
+      case 3: return Colors.pink;
+      default: return Colors.red;
+    }
+  }
+
+  IconData? _changeAppBarIcon() {
+    switch(_currentIndex) {
+      case 0: return Icons.ac_unit;
+      case 1: return Icons.work;
+      case 2: return Icons.flip;
+      case 3: return CupertinoIcons.textformat_123;
+      default: return Icons.workspaces_filled;
     }
   }
 
 
 
-  _updateImgWidget() async {
-    final ByteData data = await rootBundle.load('images/flip-${
-            Random().nextInt(2)}.gif');
-    setState(() {
-      _pic = Image.memory(data.buffer.asUint8List(), scale: 0.5,);
-    });
+  @override
+  void initState() {
+    super.initState();
+    _loadScreen();
+    setState() {
+      print('refreshing');
+    }
+  }
+
+  void _loadScreen() {
+    switch(_currentIndex) {
+      case 0: return setState(() => _currentWidget = DiceApp());
+      case 1: return setState(() => _currentWidget = DNDApp());
+      case 2: return setState(() => _currentWidget = FlipApp());
+      case 3: return setState(() => _currentWidget = RNGApp());
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     var diceColor = Colors.red;
+
     return Scaffold(
       appBar: AppBar(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(10),
-          ),
-        ),
-        backgroundColor: diceColor,
-        leading: const Icon(Icons.games),
-        title: Text(widget.title),
-        actions: [
-          IconButton(icon: const Icon(Icons.settings), onPressed: () {}),
-        ],
-      ),
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              InkWell(
-                enableFeedback: false,
+        backgroundColor: _changeAppBarColor(),
+        title: Text(_changeAppBarTitle()),
+        leading: IconButton(
+          icon: Icon(_changeAppBarIcon()),
+          onPressed: () {
 
-          child: _pic,
-          onTap: () {
-            _updateImgWidget();
           },
-            ),
-                    ]),
+          iconSize: 30,
+          splashColor: Colors.transparent,
+          alignment: Alignment.center,
+        ),
       ),
+      body: _currentWidget,
       bottomNavigationBar: BottomNavigationBar(
-        items: <BottomNavigationBarItem>[
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() => _currentIndex = index);
+          _loadScreen();
+        },
+                   items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Image.asset(
-              'images/dice64.png',
+              'images/icons/dice64.png',
             ),
             label: '',
             backgroundColor: diceColor,
           ),
           BottomNavigationBarItem(
-            icon: Image.asset('images/dndicon64.png'),
+            icon: Image.asset('images/icons/dndicon64.png'),
             label: '',
             backgroundColor: Colors.green,
           ),
           BottomNavigationBarItem(
             icon: Image.asset(
-              'images/flipco.png',
+              'images/icons/flipco.png',
             ),
             label: '',
             backgroundColor: Colors.purple,
           ),
           BottomNavigationBarItem(
             icon: Image.asset(
-              'images/raffle.png',
+              'images/icons/raffle.png',
             ),
             label: '',
             backgroundColor: Colors.pink,
           ),
         ],
-        selectedItemColor: Colors.amber[800],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
       ),
     );
   }
 
-  void _onItemTapped(int value) {
-    setState(() {
-      _selectedIndex = value;
-    });
-  }
+
+
+
+
+
 }
